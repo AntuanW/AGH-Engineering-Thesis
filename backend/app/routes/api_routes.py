24108d7cf58665fd7821ed42fd3366308a953d86
Antoni Wójcik <wojcikantek@gmail.com>
2024-06-06 10:26:13 +0200

    endpoint init

--- backend/app/routes/api_routes.py
+++ backend/app/routes/api_routes.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile, HTTPException, File
+from fastapi.responses import JSONResponse
 
 router = APIRouter()
 
@@ -6,3 +7,18 @@ router = APIRouter()
 @router.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@router.post("/upload-pkt-file")
+def upload_pkt_file(file: UploadFile = File(...)):
+    if not file.filename.endswith(".pkt"):
+        raise HTTPException(
+            status_code=400,
+            detail="File must be a .pkt file"
+        )
+    
+    # Process a file
+
+    return JSONResponse(
+        status_code=200,
+        content={"message": "File uploaded successfully"}
+    )
