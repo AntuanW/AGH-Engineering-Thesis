04bc615c5f99132b0f8b1a2a5ba9bc7af05a0bda
bartn1k <bartek.slupik@gmail.com>
2024-10-22 18:52:52 +0200

    basic file transfer

--- backend/app/routes/api_routes.py
+++ backend/app/routes/api_routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, File, UploadFile, status, Response
 
 router = APIRouter()
 
@@ -6,3 +6,18 @@ router = APIRouter()
 @router.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@router.post("/decrypt")
+def decrypt_pkt(response: Response, file: UploadFile = File(...)):
+    try:
+        name = file.filename
+        contents = file.file.read()
+
+
+    except Exception as ex:
+        response.status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
+        response.body = {"message": f"Error while reading file: {ex}"}
+
+    finally:
+        file.close()
