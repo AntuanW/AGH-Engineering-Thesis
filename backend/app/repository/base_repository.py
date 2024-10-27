9056b6a7ae6ac1dac42f8cdd8675d4717808daf9
Antoni Wojcik <wojcikantek@gmail.com>
2024-10-27 14:31:59 +0100

    fixing repository imports

--- backend/app/repository/base_repository.py
+++ backend/app/repository/base_repository.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 from pymongo import MongoClient
-from config import MONGODB_URI
+from .config import MONGODB_URI
 
 
 class BaseRepository(ABC):
--- backend/app/repository/device_repository.py
+++ backend/app/repository/device_repository.py
@@ -1,4 +1,4 @@
-from base_repository import BaseRepository
+from .base_repository import BaseRepository
 from ..models.device import DeviceModel
 
 
