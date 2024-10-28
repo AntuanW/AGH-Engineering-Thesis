16d3a98320e5fe51f80e5e39a6ad8b83ad7787eb
Antoni Wojcik <wojcikantek@gmail.com>
2024-10-28 17:46:32 +0100

    pytest init, error handling

--- backend/app/repository/device_repository.py
+++ backend/app/repository/device_repository.py
@@ -1,11 +1,12 @@
 from .base_repository import BaseRepository
 from ..models.device import DeviceModel
+from pymongo.collection import Collection
 
 
 class DeviceRepository(BaseRepository):
 
-    def get_collection(self):
-        return self.db['devices']
+    def get_collection(self) -> Collection:
+        return self.db.get_collection('devices')
 
     def insert(self, device: DeviceModel):
         return super().insert(device.model_dump())
---
+++ backend/app/repository/exceptions/repository_exceptions.py
@@ -0,0 +1,26 @@
+class DotEnvException(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return self.message
+
+
+class URIException(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return self.message
+
+
+class DatabaseException(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return self.message
+
