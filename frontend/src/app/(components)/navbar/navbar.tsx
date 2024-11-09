2f103a3d32a699170fc635d29cec9f84417c4ff1
Grzegorz <grzegorzpiskorski971@gmail.com>
2024-11-09 18:33:35 +0100

    Dynamic highlighted navbar

--- frontend/src/app/(components)/navbar/navbar.tsx
+++ frontend/src/app/(components)/navbar/navbar.tsx
@@ -1,21 +1,34 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import "./navbar.css";
 
 const Navbar = () => {
+  const pathname = usePathname();
+  const isActive = (href: string) => pathname === href ? "single-link active" : "single-link";
+
   return (
     <div className="nav-container">
       <ul className="links-container">
         <li className="single-link-container">
-          <Link href="/" className="single-link">Home</Link>
+          <Link href="/" className={isActive("/")}>
+            Home
+          </Link>
         </li>
         <li className="single-link-container">
-          <Link href="/upload-config" className="single-link">Upload config</Link>
+          <Link href="/upload-config" className={isActive("/upload-config")}>
+            Upload config
+          </Link>
         </li>
         <li className="single-link-container">
-          <Link href="/download-config" className="single-link">Download config</Link>
+          <Link href="/download-config" className={isActive("/download-config")}>
+            Download config
+          </Link>
         </li>
         <li className="single-link-container">
-          <Link href="/manage-devices" className="single-link">Manage devices in lab</Link>
+          <Link href="/manage-devices" className={isActive("/manage-devices")}>
+            Manage devices in lab
+          </Link>
         </li>
       </ul>
     </div>
