a0895ba1efdce8ffc283cae9d584348a9bdca982
Antoni Wojcik <wojcikantek@gmail.com>
2024-11-22 11:51:58 +0100

    tests

--- backend/app/repository/device_repository.py
+++ backend/app/repository/device_repository.py
@@ -1,3 +1,5 @@
+from bson import ObjectId
+
 from .base_repository import BaseRepository
 from ..models.device import DeviceModel
 from pymongo.collection import Collection
@@ -10,3 +12,10 @@ class DeviceRepository(BaseRepository):
 
     def insert(self, device: DeviceModel):
         return super().insert(device.model_dump())
+
+    def find_by_id(self, device_id: ObjectId) -> DeviceModel | None:
+        device_dict: dict = self.find_one({'_id': device_id})
+        print(device_dict)
+        if not device_dict:
+            return None
+        return DeviceModel(**device_dict)
