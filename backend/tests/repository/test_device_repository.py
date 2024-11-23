8a3f35eb5a5228b6ff24a727a2a09a329ac6beec
Antoni Wójcik <92227211+AntuanW@users.noreply.github.com>
2024-11-23 15:09:27 +0100

    device-repository (#33)
    * tests
    
    * print fix

--- backend/tests/repository/test_device_repository.py
+++ backend/tests/repository/test_device_repository.py
@@ -1,5 +1,15 @@
+from bson import ObjectId
+
 from app.repository.device_repository import DeviceRepository
+from app.models.device import DeviceModel
 
 def test_basic_connection():
     repo = DeviceRepository()
     repo.get_collection()
+
+
+def test_find_by_id():
+    repo = DeviceRepository()
+    device = repo.find_by_id(ObjectId('671b941494283ece5865951b'))
+    assert device is not None
+    assert type(device) is DeviceModel
