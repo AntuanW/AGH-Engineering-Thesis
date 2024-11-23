41895d430162f2578edcc5988bb620035b472705
Antoni Wojcik <wojcikantek@gmail.com>
2024-11-23 19:02:33 +0100

    how to mock tutorial, may be useful later (preparing chapter III)

--- backend/tests/repository/test_device_repository.py
+++ backend/tests/repository/test_device_repository.py
@@ -1,4 +1,5 @@
 from bson import ObjectId
+from unittest.mock import MagicMock
 
 from app.repository.device_repository import DeviceRepository
 from app.models.device import DeviceModel
@@ -9,7 +10,19 @@ def test_basic_connection():
 
 
 def test_find_by_id():
+    mock_device_id = ObjectId('5f8f8f8f8f8f8f8f8f8f8f8f')
+    mock_device_data = {
+        '_id': mock_device_id,
+        'name': 'test_name',
+        'type': 'test_type'
+    }
+    expected_device = DeviceModel(**mock_device_data)
+
     repo = DeviceRepository()
-    device = repo.find_by_id(ObjectId('671b941494283ece5865951b'))
-    assert device is not None
-    assert type(device) is DeviceModel
+    repo.find_one = MagicMock(return_value=mock_device_data)
+
+    found_device = repo.find_by_id(mock_device_id)
+
+    assert found_device is not None
+    assert found_device == expected_device
+    assert isinstance(found_device, DeviceModel)
