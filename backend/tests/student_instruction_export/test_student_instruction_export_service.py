100610a4184492193aa82c8a4320e71dc89b57e1
zolszowka <zuzanna@olszowka.net>
2024-11-30 18:16:19 +0100

    add tests

--- backend/tests/student_instruction_export/test_student_instruction_export_service.py
+++ backend/tests/student_instruction_export/test_student_instruction_export_service.py
@@ -1,79 +1,13 @@
 import unittest
 
-from pydantic import IPvAnyAddress
-
-from app.config_upload.util.netmiko_types import NetmikoDeviceType
-from app.models.connection import ConnectionModel
-from app.student_instruction_export.student_instruction_export_service import InstructionExportService
-from app.models.mapping import MappingModel
-from app.models.mapped_device import MappedDeviceModel
+from app.repository.mapping_repository import MappingRepository
+from app.student_instruction_export.student_instruction_export_service import StudentInstructionExportService
 
 
 class TestStudentInstructionExportService(unittest.TestCase):
     def test_export_instructions(self):
-        mock_mappings = [
-            MappingModel(lab_group=1, mapped_devices=[
-                MappedDeviceModel(
-                    name='R11',
-                    netmiko_device_type=NetmikoDeviceType('cisco_ios'),
-                    ip_address=IPvAnyAddress('172.17.145.10'),
-                    port=2001,
-                    neighbours=[
-                        ConnectionModel(
-                            neighbour_name='R12',
-                            from_interface='GigabitEthernet0/0',
-                            to_interface='GigabitEthernet0/0',
-                        )
-                    ],
-                    mapped_config=['show version', 'show running-config'],
-                ),
-                MappedDeviceModel(
-                    name='R12',
-                    netmiko_device_type=NetmikoDeviceType('cisco_ios'),
-                    ip_address=IPvAnyAddress('172.17.145.10'),
-                    port=2002,
-                    neighbours=[
-                        ConnectionModel(
-                            neighbour_name='R11',
-                            from_interface='GigabitEthernet0/0',
-                            to_interface='GigabitEthernet0/0',
-                        )
-                    ],
-                    mapped_config=['show version', 'show running-config'],
-                ),
-            ]),
-            MappingModel(lab_group=2, mapped_devices=[
-                MappedDeviceModel(
-                    name='R21',
-                    netmiko_device_type=NetmikoDeviceType('cisco_ios'),
-                    ip_address=IPvAnyAddress('172.17.145.10'),
-                    port=2001,
-                    neighbours=[
-                        ConnectionModel(
-                            neighbour_name='R22',
-                            from_interface='GigabitEthernet0/0',
-                            to_interface='GigabitEthernet0/0',
-                        )
-                    ],
-                    mapped_config=['show version', 'show running-config'],
-                ),
-                MappedDeviceModel(
-                    name='R22',
-                    netmiko_device_type=NetmikoDeviceType('cisco_ios'),
-                    ip_address=IPvAnyAddress('172.17.145.10'),
-                    port=2002,
-                    neighbours=[
-                        ConnectionModel(
-                            neighbour_name='R21',
-                            from_interface='GigabitEthernet0/0',
-                            to_interface='GigabitEthernet0/0',
-                        )
-                    ],
-                    mapped_config=['show version', 'show running-config'],
-                ),
-            ])
-        ]
-
-        instruction_export_service = InstructionExportService()
-        instruction_export_service.export_instructions(mock_mappings)
-        assert True
+        mapping_repo = MappingRepository()
+        mappings = mapping_repo.find_objects({})
+        instruction_export_service = StudentInstructionExportService()
+        filename = instruction_export_service.export_instructions(mappings)
+        self.assertTrue(filename)
--- backend/tests/student_instruction_export/test_student_instruction_pdf_generator.py
+++ backend/tests/student_instruction_export/test_student_instruction_pdf_generator.py
@@ -1,12 +1,12 @@
 import unittest
-from unittest.mock import MagicMock
 
-from app.student_instruction_export.student_instruction_pdf_generator import InstructionPdfGenerator
+from app.student_instruction_export.student_instruction_pdf_generator import StudentInstructionPdfGenerator
+from reportlab.platypus import Paragraph
 
 
 class TestStudentInstructionPdfGenerator(unittest.TestCase):
     def test_generate_pdf(self):
-
-        instruction_pdf_generator = InstructionPdfGenerator()
-        instruction_pdf_generator.generate_pdf()
-        assert True
+        content = [Paragraph("TEST")]
+        instruction_pdf_generator = StudentInstructionPdfGenerator()
+        instruction_pdf_generator.generate_pdf(content)
+        self.assertTrue(instruction_pdf_generator.filename)
