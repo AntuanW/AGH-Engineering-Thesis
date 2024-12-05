7f9875ed64d4affd2e87b5e2b2df54134455e3cc
oilycannelloni <bartek.slupik@gmail.com>
2024-12-05 09:46:42 +0100

    pc insert

--- backend/app/models/mapped_device.py
+++ backend/app/models/mapped_device.py
@@ -1,11 +1,12 @@
 from pydantic import BaseModel
-from pydantic.networks import IPvAnyAddress
+from .device import DeviceType
 from .connection import ConnectionModel
 from ..config_upload.util.netmiko_types import NetmikoDeviceType
 
 
 class MappedDeviceModel(BaseModel):
     name: str
+    device_type: DeviceType
     netmiko_device_type: NetmikoDeviceType
     ip_address: str
     port: int
