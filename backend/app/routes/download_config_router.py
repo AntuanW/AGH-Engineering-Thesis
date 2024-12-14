58f14970bc078791c7926714fb04514b4c3c8e91
Grzegorz <grzegorzpiskorski971@gmail.com>
2024-12-14 09:24:50 +0100

    Code cleanup

--- backend/app/routes/download_config_router.py
+++ backend/app/routes/download_config_router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, status
-from fastapi.responses import FileResponse, JSONResponse, Response
+from fastapi.responses import Response
 
 from app.config_download.config_download_service import ConfigDownloadService
 from app.config_download.utils.download_config_request import DownloadConfigRequest
@@ -18,8 +18,4 @@ async def download_configs(
 
     download_results: list[DownloadedConfig] = config_download_service.download_devices_config(download_request)
 
-    # TODO: reversed mapping to some packet tracer device
-    # ...
-    # TODO: pdf generation and preparing the file to download
-    # return type will be changed after the above changes
     return Response(status_code=status.HTTP_204_NO_CONTENT)
