c80a1ec7d607058c0fc22cb5324939366a1c409a
Zuzanna Olszówka <92948112+zolszowka@users.noreply.github.com>
2024-12-23 23:58:25 +0100

    Add logs (#78)
    * add logs for instructions
    
    * add logs for mapping and change logs for instructions
    
    * few changes
    
    * test

--- backend/tests/visualization/test_topology_visualizer.py
+++ backend/tests/visualization/test_topology_visualizer.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 from reportlab.lib.pagesizes import A4
 from reportlab.platypus import SimpleDocTemplate, Spacer
@@ -7,15 +8,21 @@ from app.models.connection import ConnectionModel
 
 
 class TestTopologyVisualizer(unittest.TestCase):
+    PDF_FILENAME = "pdf_files/test_topology_visualization.pdf"
+
+    def setUp(self):
+        if os.path.exists(self.PDF_FILENAME):
+            os.remove(self.PDF_FILENAME)
+
     def generate_pdf_with_image(self, image):
-        pdf_file = "pdf_files/test_topology_visualization.pdf"
-        doc = SimpleDocTemplate(pdf_file, pagesize=A4)
+        doc = SimpleDocTemplate(self.PDF_FILENAME, pagesize=A4)
 
         content = [image, Spacer(1, 12)]
         doc.build(content)
 
     def test_draw_graph(self):
-        devices = {'S11': 'SWITCH', 'S12': 'SWITCH', 'S13': 'SWITCH', 'R11': 'ROUTER', 'R12': 'ROUTER', 'R13': 'ROUTER', 'R14': 'ROUTER'}
+        devices = {'S11': 'SWITCH', 'S12': 'SWITCH', 'S13': 'SWITCH', 'R11': 'ROUTER', 'R12': 'ROUTER', 'R13': 'ROUTER',
+                   'R14': 'ROUTER'}
         connections = [
             ConnectionModel(origin_name='S11', neighbour_name='R11', from_interface='FastEthernet0/0',
                             to_interface='GigabitEthernet0/1/1'),
@@ -39,6 +46,8 @@ class TestTopologyVisualizer(unittest.TestCase):
         image = visualizer.draw_graph(graph)
         self.generate_pdf_with_image(image)
 
+        assert os.path.isfile(self.PDF_FILENAME)
+
     def test_draw_graph_pc(self):
         devices = {'S11': 'SWITCH', 'S12': 'SWITCH', 'S13': 'SWITCH', 'R11': 'ROUTER', 'K11': 'PC', 'K12': 'PC'}
 
@@ -59,3 +68,5 @@ class TestTopologyVisualizer(unittest.TestCase):
         graph = visualizer.generate_graph()
         image = visualizer.draw_graph(graph)
         self.generate_pdf_with_image(image)
+
+        assert os.path.isfile(self.PDF_FILENAME)
