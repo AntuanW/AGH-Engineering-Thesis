a8a16a55f97c4d1b5141d9b56375bc85f43f17f7
Grzegorz <grzegorzpiskorski971@gmail.com>
2024-12-23 20:42:07 +0100

    Added metadata and changed readme

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,11 @@
+import type { Metadata } from "next";
 import Navbar from "./(components)/navbar/navbar";
 import "./globals.css";
 
+export const metadata: Metadata = {
+  title: "Network Lab Orchestrator",
+  description: "Simple ui for orchestrating network lab"
+};
 
 export default function RootLayout({
   children,
