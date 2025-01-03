83b998aaf31e8b0c27436ad3429c7ba69b03aa60
zolszowka <zuzanna@olszowka.net>
2025-01-03 19:20:06 +0100

    test for PCs fixed

--- backend/tests/running_config/TestBasicConfigExtractor.py
+++ backend/tests/running_config/TestBasicConfigExtractor.py
@@ -1,14 +1,9 @@
-#TODO - config extraction tests fix
 import unittest
 import xmltodict
 from pathlib import Path
 
-from bson import ObjectId
-
-from app.repository.decrypted_xml_repository import DecryptedXMLRepository
 from app.running_config.config_extractor import ConfigExtractor
 from app.running_config.util.device_config_types import DeviceConfigInfo, DeviceType
-from app.running_config.exceptions.config_extraction_exceptions import XmlOpenException
 from app.running_config.util.device_config_types import XmlConfigConstants
 
 RESOURCES_PATH = Path(__file__).parent
@@ -34,7 +29,7 @@ class TestBasicConfigExtractor(unittest.TestCase):
         assert device_counter[DeviceType.PC] == 0, "Switches counter is not equal to 0"
 
     def test_get_topology_config_from_xml_pc(self):
-        xml_file_path: Path = RESOURCES_PATH.joinpath('resources/test-topology.xml')
+        xml_file_path: Path = RESOURCES_PATH.joinpath('resources/test-topology-pc.xml')
         with open(xml_file_path, "r") as file:
             xml = file.read()
             xml_dict: dict = xmltodict.parse(xml)
@@ -46,7 +41,7 @@ class TestBasicConfigExtractor(unittest.TestCase):
         for dev in result:
             device_counter[dev.dev_type] += 1
 
-        assert len(result) == 9, f"Failed to parse xml - invalid number of devices found: 6 != {len(result)}"
-        assert device_counter[DeviceType.ROUTER] == 1, "Routers counter is not equal to 1"
-        assert device_counter[DeviceType.SWITCH] == 3, "Switches counter is not equal to 3"
-        assert device_counter[DeviceType.PC] == 2, "Switches counter is not equal to 2"
+        assert len(result) == 7, f"Failed to parse xml - invalid number of devices found: 7 != {len(result)}"
+        assert device_counter[DeviceType.ROUTER] == 3, "Routers counter is not equal to 3"
+        assert device_counter[DeviceType.SWITCH] == 1, "Switches counter is not equal to 1"
+        assert device_counter[DeviceType.PC] == 3, "Switches counter is not equal to 3"
